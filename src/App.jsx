5d0bbf5ec749e6e6df549dfa450ff57d15471ea5
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-01-23 23:08:53 +0530

    create paes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
 
 const App = () => {
-  return <div className="text-red-500"> App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
