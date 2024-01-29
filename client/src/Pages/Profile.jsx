52abe6b21588cf84cc5852e34528212416c32bdc
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-01-29 08:01:01 +0530

    complete profile page

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user.user);
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form action="" className="flex flex-col gap-4">
+        <img
+          className="w-24 h-24 object-cover mt-2 self-center cursor-pointer rounded-full"
+          src={currentUser.avatar}
+          alt="user image"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between my-5">
+        <span className="text-red-700">Delete Account</span>
+        <span className="text-blue-700">Sign Out </span>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
