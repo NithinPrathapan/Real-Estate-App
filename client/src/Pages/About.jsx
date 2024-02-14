d3934764df23543f100af4c0ddb5dedda8c0132e
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-02-14 08:01:07 +0530

    complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Vitalys Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Vitalys Estate is a leading Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Officia sequi nesciunt exercitationem sed ad tempora
+        alias voluptas. Optio, earum praesentium numquam sunt quisquam, maxime
+        sit eligendi magnam dolorum nobis est voluptas voluptatum, consectetur
+        soluta accusamus nulla quas maiores placeat. Dolores non perspiciatis
+        necessitatibus voluptate dolorum ullam, esse similique atque temporibus
+        eaque animi! Sequi deserunt fugit mollitia dignissimos reiciendis amet
+        omnis.{" "}
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae dolor
+        rem eveniet quasi voluptatem at magni culpa, vel distinctio, ducimus
+        accusantium minima repellendus totam aut temporibus enim blanditiis!
+        Fugiat eius consectetur iure. Dignissimos eaque aspernatur repellat
+        deserunt hic saepe in!
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        maiores quae ea ab maxime fugit nulla assumenda laboriosam. Odio facilis
+        corporis laborum ratione nam perferendis odit perspiciatis, eaque minima
+        vero!
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
