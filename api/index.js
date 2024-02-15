1ac48a72e1c757c3814b2d90ccde16758bbe5241
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-02-15 05:59:19 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@ import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 const corsOptions = {
   origin: "http://localhost:3000",
@@ -22,10 +25,15 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser(process.env.SECRET, { secure: true }));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
