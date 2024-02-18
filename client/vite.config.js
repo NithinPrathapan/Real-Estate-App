9c1192f7e9cde077955c656bfe6bf7550a10878f
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-02-18 10:26:23 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
     proxy: {
-      "/api": "https://mern-estate-c7gd.onrender.com",
+      '/api': {
+        target: 'http://localhost:8000',
+        secure: false,
+      },
     },
   },
+
   plugins: [react()],
 });
